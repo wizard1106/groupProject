e22549155af84a13ce5b49cebf54dbaa53fd8ee2
StandsandLooksBack,Andrew <116851465+andrewstands@users.noreply.github.com>
2024-02-07 19:04:23 -0600

    combine.js

--- combine.js
+++ combine.js
@@ -1,1 +1,14 @@
+// combined.js
+import('./HelloAndrew.js').then(({ default: helloAndrew }) => {
+    import('./helloBrennan.js').then(({ default: helloBrennan }) => {
+      import('./helloCasey.js').then(({ default: helloCasey }) => {
+        import('./helloPaul.js').then(({ default: helloPaul }) => {
+          // Combine the contents of all files
+          const combinedContent =' ';
 
+          // Print combined content
+          console.log(combinedContent);
+        });
+      });
+    });
+  });
