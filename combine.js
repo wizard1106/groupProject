ada5118f634d28355c602d74dc8d5991465f0bde
StandsandLooksBack,Andrew <116851465+andrewstands@users.noreply.github.com>
2024-02-07 19:00:55 -0600

    Update combine.js

--- combine.js
+++ combine.js
@@ -1,18 +1,1 @@
-// A program that runs // main.js
-const helloAndrew = require('./HelloAndrew');
-const helloBrennan = require('./helloBrennan');
-const helloCasey = require('./helloCasey');
-const helloPaul = require('./helloPaul');
 
-// Call the functions to display their output
-console.log('Output of HelloAndrew:');
-helloAndrew();
-
-console.log('Output of helloBrennan:');
-helloBrennan();
-
-console.log('Output of helloCasey:');
-helloCasey();
-
-console.log('Output of helloPaul:');
-helloPaul();all of our js files that prints our names
